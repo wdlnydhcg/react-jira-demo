3946b1903d250491132ba5677e8ea972edc99c68
zhong mingyuan <mralenzhong@qq.com>
2022-05-18 22:11:08 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: MrAlenZhong
  * @Date: 2022-05-18 08:42:14
  * @LastEditors: MrAlenZhong
- * @LastEditTime: 2022-05-18 22:09:03
+ * @LastEditTime: 2022-05-18 22:11:02
  * @Description:
  */
 import React from "react";
