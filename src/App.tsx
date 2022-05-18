e01c3a8c2515e03404454d4b1faade7f8dff4e4e
zhong mingyuan <mralenzhong@qq.com>
2022-05-18 22:10:41 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Author: MrAlenZhong
+ * @Date: 2022-05-18 08:42:14
+ * @LastEditors: MrAlenZhong
+ * @LastEditTime: 2022-05-18 22:09:03
+ * @Description:
+ */
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
