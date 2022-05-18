41bf97c9d85d40f84d468954643da2fae71bac61
zhong mingyuan <mralenzhong@qq.com>
2022-05-18 22:49:36 +0800

    fix: add husky

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: MrAlenZhong
+ * @Date: 2022-05-18 22:47:20
+ * @LastEditors: MrAlenZhong
+ * @LastEditTime: 2022-05-18 22:47:29
+ * @Description:
+ */
+module.exports = { extends: ["@commitlint/config-conventional"] };
